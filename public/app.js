3c6a307db25cfa7b25450fdf9bdeb5f9061b5d32
Elgrogz <gregorgilchrist@gmail.com>
2017-01-23 17:00:20 +0000

    css added

--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
-var catArray = [{name: "Boba", favFood: "Sock Fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"}, {name: "Barnaby", favFood: "Tuna", image: "http://65.media.tumblr.com/8a827e13ebb5db7d16e2b1c4cbe7ce70/tumblr_odtp4ftIhB1uhevdso1_400.jpg"}, {name: "Max", favFood: "Whiskas Tempations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}];
+var catArray = [{name: "Boba", favFood: "Sock Fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"}, {name: "Barnaby", favFood: "Tuna", image: "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg"}, {name: "Max", favFood: "'Whiskas Tempations'", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}];
 
+
+
+var Cat = function(name, favFood, imageSource) {
+  this.name = name;
+  this.favFood = favFood;
+  this.imageSource = imageSource;
+}
+
+var addCatToArray = function(cat) {
+  catArray.push(cat); 
+}
+
+var createLine = function(text) {
+  var catLine = document.createElement('li');
+  catLine.innerText = text;
+  return catLine;
+}
 
 var createCatProfile = function() {
   var catProfile = document.createElement('ul');
@@ -8,42 +25,39 @@ var createCatProfile = function() {
 }
 
 var createCatName = function(name) {
-  var catName = document.createElement('li');
-  catName.innerText = name;
-  return catName;
+  return createLine("Name: " + name);
 }
 
 var createCatFavFood = function(favFood) {
-  var catFavFood = document.createElement('li');
-  catFavFood.innerText = favFood;
-  return catFavFood;
+  return createLine("Favourite Food: " + favFood);
 }
 
-// var createCatImage = function(imageSource) {
-//   var catImage = document.createElement('img');
-//   catImage.innerText = imageSource;
-//   return catImage;
-// }
+var createCatImage = function(imageSource) {
+  var catImage = document.createElement('img');
+  catImage.src = imageSource;
+  catImage.width = "400";
+  return catImage;
+}
 
-var appendElements = function(cats, catProfile, catName, catFavFood) {
+var appendElements = function(cats, catProfile, catName, catFavFood, catImage) {
   catProfile.appendChild(catName);
   catProfile.appendChild(catFavFood);
-  // catProfile.appendChild(catImage);
+  catProfile.appendChild(catImage);
   cats.appendChild(catProfile);
 }
 
-var addCat = function(name, favFood) {
+var addCat = function(name, favFood, imageSource) {
   var catProfile = createCatProfile();
   var catName = createCatName(name);
   var catFavFood = createCatFavFood(favFood);
-  // var catImage = createCatImage(imageSource);
+  var catImage = createCatImage(imageSource);
   var cats = document.getElementById('cats');
-  appendElements(cats, catProfile, catName, catFavFood);
+  appendElements(cats, catProfile, catName, catFavFood, catImage);
 } 
 
 var app = function() {
   for (var cat of catArray) {
-    addCat(cat.name, cat.favFood)
+    addCat(cat.name, cat.favFood, cat.image)
   }
 }
 
